04c894fc2b7c0ba75a79f56e269fe426129915ca
zwopac11 <zwopac11@htlkaindorf.at>
2015-04-17 08:35:16 +0200

    test

---
+++ src/Test/FrameController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Paul
+ */
+public class FrameController implements Initializable {
+
+    @FXML
+//    private TextField tfName;
+    //Definition des Action-Events:
+//    @FXML
+//    public void onHallo(ActionEvent evt)
+//    {
+//        System.out.println("Hallo");
+//        System.out.println(tfName.getText());
+//    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
